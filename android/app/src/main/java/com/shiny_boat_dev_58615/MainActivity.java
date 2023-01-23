b6f5c36dffc7c081985dc544cf4864580c635a47
Crowdbotics <engineering@crowdbotics.com>
2023-01-23 00:07:42 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/shiny_boat_dev_58615/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shiny_boat_dev_58615;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "shiny_boat_dev_58615";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shiny-boat-dev-58615",
+  url: "https://shiny-boat-dev-58615.botics.co",
+  api: "https://shiny-boat-dev-58615.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
